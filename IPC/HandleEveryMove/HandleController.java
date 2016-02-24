2cd87527e9c374f78116aad00c0aab2c68b12c70
Carlos Galindo <carlos.s.galindo@gmail.com>
2016-02-24 21:19:15 +0100

    IPC HandleMove: files init

---
+++ IPC/HandleEveryMove/HandleController.java
@@ -0,0 +1,19 @@
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * FXML Controller class
+ *
+ * @author Carlos
+ */
+public class HandleController implements Initializable {
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+}
