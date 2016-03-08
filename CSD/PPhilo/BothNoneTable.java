ebdfa3fd2b63ce714b6c734877c27ab2a7181d35
Carlos Galindo <carlos.s.galindo@gmail.com>
2016-03-08 08:25:56 +0100

    Begin working on PPhilo (CSD Lab2)

---
+++ CSD/PPhilo/BothNoneTable.java
@@ -0,0 +1,5 @@
+// CSD Mar 2013 Juansa Sendra
+ 
+public class BothNoneTable extends RegularTable { //both or none
+	public BothNoneTable(Log log) {super(log);}
+}
---
+++ CSD/PPhilo/LefthandPhilo.java
@@ -0,0 +1,12 @@
+// CSD Mar 2013 Juansa Sendra
+ 
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.event.*;
+import java.util.*;
+
+public class LefthandPhilo extends Philo { //ASSIMETRY
+	public LefthandPhilo(int id, int cycles, int delay, Table table) {super(id,cycles,delay,table);}
+}
