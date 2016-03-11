b2da384169536dd0e9d6b88c92dd39cf4ecc8674
Carlos Galindo <carlos.s.galindo@gmail.com>
2016-03-11 13:36:25 +0100

    ListaPersonas small changes (doesn't compile)

--- IPC/ListaPersonas/controlador/VistaListaControlador.java
+++ IPC/ListaPersonas/controlador/VistaListaControlador.java
@@ -8,7 +8,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -19,12 +18,11 @@ import modelo.Persona;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class VistaListaControlador implements Initializable {
 
-    private ObservableList<Persona> datos = null; // Colecci�n vinculada a la vista.
+    private ObservableList<Persona> datos = null; // Colección vinculada a la vista.
 
     @FXML
     private ListView<Persona> listView;
@@ -37,12 +35,12 @@ public class VistaListaControlador implements Initializable {
     final private SimpleBooleanProperty apellidos = new SimpleBooleanProperty(true);
 
     @FXML
-    void addAccion(ActionEvent event) {
-        // Codigo para mostrar la ventana de edicion
+    protected void addAccion(ActionEvent event) {
+        // Código para mostrar la ventana de edición
         try {
 
             FXMLLoader cargador = new FXMLLoader(getClass().getResource("/vista/VistaEditar.fxml"));
-            Parent root = (Parent) cargador.load();
+            Parent root = cargador.load();
 
             ((VistaEditarControlador) cargador.getController()).setList(datos);
 
@@ -60,22 +58,22 @@ public class VistaListaControlador implements Initializable {
     }
     
     @FXML
-    void duplicarAccion(ActionEvent event) {
+    protected void duplicarAccion(ActionEvent event) {
         datos.addAll(listView.getSelectionModel().getSelectedItems());
     }
 
     @FXML
-    void borrarAccion(ActionEvent event) {
+    protected void borrarAccion(ActionEvent event) {
         datos.removeAll(listView.getSelectionModel().getSelectedItems());
     }
     
     @FXML
-    void editarAccion(ActionEvent event) {
+    protected void editarAccion(ActionEvent event) {
         int i = listView.getSelectionModel().getSelectedIndex();
         if (i >= 0) {
             try {
                 FXMLLoader cargador = new FXMLLoader(getClass().getResource("/vista/VistaEditar.fxml"));
-                Parent root = (Parent) cargador.load();
+                Parent root = cargador.load();
 
                 ((VistaEditarControlador) cargador.getController()).setList(datos, i);
 
@@ -93,14 +91,14 @@ public class VistaListaControlador implements Initializable {
     }
     
     @FXML
-    void mostrarAccion(ActionEvent event) {
-        String id = ((Node) event.getSource()).getId();
+    protected void mostrarAccion(ActionEvent event) {
+        String id = ((Menu) event.getSource()).getId();
         apellidos.set(menuOrdenReverso.getId().equals(id));
     }
     
     @FXML
-    void ordenarAccion(ActionEvent event) {
-        listView.getItems().sort(
+    protected void ordenarAccion(ActionEvent event) {
+        datos.sort(
                 (p1, p2) -> {
                     int res;
                     if (apellidos.get()) {
@@ -121,7 +119,7 @@ public class VistaListaControlador implements Initializable {
     void moverAccion(ActionEvent event) {
         int index = listView.getSelectionModel().getSelectedIndex();
         Persona persona = datos.remove(index);
-        if (((Button) event.getSource()).equals(subirButton)) index--;
+        if (event.getSource().equals(subirButton)) index--;
         else index++;
         datos.add(index, persona);
         listView.getSelectionModel().clearAndSelect(index);
@@ -139,9 +137,9 @@ public class VistaListaControlador implements Initializable {
         misdatos.add(new Persona("Pepe", "García"));
         misdatos.add(new Persona("María", "Pérez"));
         datos = FXCollections.observableArrayList(misdatos);
-        listView.setItems(datos); // vinculaci�n entre la vista y el modelo
+        listView.setItems(datos); // vinculación entre la vista y el modelo
         
-        /* asignar aqui el nuevo estilo de la celda*/
+        /* asignar aquí el nuevo estilo de la celda*/
         /*-------------------------------------------------------*/
         listView.setCellFactory(c -> new PersonListCell());
         listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
@@ -162,7 +160,7 @@ public class VistaListaControlador implements Initializable {
         });
         apellidos.addListener(
                 (observer, oldValue, newValue) -> {
-                    if (!Objects.equals(newValue, oldValue)) listView.refresh();
+                    datos.
                 }
         );
     }
