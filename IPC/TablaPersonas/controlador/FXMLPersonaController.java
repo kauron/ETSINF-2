9607186d4662cc53969645947d9a74a78f8b31cd
Carlos Galindo <carlos.s.galindo@gmail.com>
2016-03-16 20:59:04 +0100

    IPC TablaPersonas: add and modify

--- IPC/TablaPersonas/controlador/FXMLPersonaController.java
+++ IPC/TablaPersonas/controlador/FXMLPersonaController.java
@@ -5,9 +5,14 @@
  */
 package TablaPersonas.controlador;
 
+import TablaPersonas.modelo.Persona;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -24,12 +29,45 @@ public class FXMLPersonaController implements Initializable {
     @FXML
     private TextField apellidosText;
 
+    ObservableList<Persona> list;
+    Persona persona;
+    int index = -1;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+
+    protected void setList(ObservableList<Persona> list) {
+        this.list = list;
+        persona = new Persona("", "");
+        persona.ApellidosProperty().bind(apellidosText.textProperty());
+        persona.NombreProperty().bind(nombreText.textProperty());
+    }
+
+    protected void setList(ObservableList<Persona> list, int index) {
+        this.index = index;
+        this.setList(list);
+        apellidosText.setText(list.get(index).getApellidos());
+        nombreText.setText(list.get(index).getNombre());
+    }
+
+    @FXML
+    protected void okAction(ActionEvent event) {
+        if (nombreText.getText().isEmpty() || apellidosText.getText().isEmpty()) return;
+        if (index == -1)
+            list.add(persona);
+        else
+            list.set(index, persona);
+        cancelAction(event);
+    }
+
+    @FXML
+    protected void cancelAction(ActionEvent event) {
+        ((Stage) ((Node) event.getSource()).getScene().getWindow()).close();
+    }
 }
--- IPC/TablaPersonas/controlador/FXMLPersonasTableController.java
+++ IPC/TablaPersonas/controlador/FXMLPersonasTableController.java
@@ -5,13 +5,24 @@
  */
 package TablaPersonas.controlador;
 
+import TablaPersonas.modelo.Persona;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 /**
@@ -28,18 +39,80 @@ public class FXMLPersonasTableController implements Initializable {
     @FXML
     private Button delButton;
     @FXML
-    private TableView<?> personasTable;
+    private TableView<Persona> personasTable;
     @FXML
-    private TableColumn<?, ?> nomColum;
+    private TableColumn<Persona, String> nomColum;
     @FXML
-    private TableColumn<?, ?> apellColum;
+    private TableColumn<Persona, String> apellColum;
+
+    private ObservableList<Persona> datos;
+
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        nomColum.setCellValueFactory(e -> e.getValue().NombreProperty());
+        apellColum.setCellValueFactory(e -> e.getValue().ApellidosProperty());
+
+        ArrayList<Persona> misdatos = new ArrayList<>();
+        misdatos.add(new Persona("Pepe", "García"));
+        misdatos.add(new Persona("María", "Pérez"));
+        datos = FXCollections.observableArrayList(misdatos);
+        personasTable.setItems(datos); // vinculación entre la vista y el modelo
+    }
+
+    @FXML
+    protected void addClick(ActionEvent event) {
+        // Mostrar la ventana de inserción
+        try {
+            FXMLLoader cargador = new FXMLLoader(getClass().getResource("/TablaPersonas/vista/FXMLPersona.fxml"));
+            Parent root = cargador.load();
+
+            ((FXMLPersonaController) cargador.getController()).setList(datos);
+
+            Scene scene = new Scene(root);
+
+            Stage ventana2 = new Stage();
+            ventana2.setTitle("Insertar registro");
+            ventana2.initModality(Modality.WINDOW_MODAL);
+            ventana2.setScene(scene);
+            ventana2.setResizable(false);
+            ventana2.show();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @FXML
+    protected void delClick(ActionEvent event) {
+        datos.remove(personasTable.getSelectionModel().getSelectedItem());
+    }
+
+    @FXML
+    protected void modClick(ActionEvent event) {
+        int index = personasTable.getSelectionModel().getSelectedIndex();
+        if (index < 0) return;
+        // Mostrar la ventana de inserción
+        try {
+            FXMLLoader cargador = new FXMLLoader(getClass().getResource("/TablaPersonas/vista/FXMLPersona.fxml"));
+            Parent root = cargador.load();
+
+            ((FXMLPersonaController) cargador.getController()).setList(datos, index);
+
+            Scene scene = new Scene(root);
+
+            Stage ventana2 = new Stage();
+            ventana2.setTitle("Insertar registro");
+            ventana2.initModality(Modality.WINDOW_MODAL);
+            ventana2.setScene(scene);
+            ventana2.setResizable(false);
+            ventana2.show();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
